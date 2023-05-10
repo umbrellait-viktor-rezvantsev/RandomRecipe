ff3a1c51b174c99a1cf5048622e27111e1af539d
Viktor Rezvantsev <viktor.rezvantsev@umbrellait.com>
2023-05-10 11:37:16 +0300

    Initial Commit

---
+++ RandomRecipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomRecipe
+//
+//  Created by Viktor Rezvantsev on 10.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
